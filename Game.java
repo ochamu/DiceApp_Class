4b7e52b53f706cf2737ca63133e0dd853bb42938
ochamu <171050612+ochamu@users.noreply.github.com>
2024-12-11 16:02:13 +0900

    クラスファイル作成

---
+++ Game.java
@@ -0,0 +1,18 @@
+public class Game {
+  Player p;
+  Dice d;
+
+  Game() {
+    p = new Player();
+    d = new Dice();
+  }
+  
+  void start() {
+    p.turn(d);
+    System.out.println(d.me);
+  }
+
+  void judge() {
+
+  }
+}
